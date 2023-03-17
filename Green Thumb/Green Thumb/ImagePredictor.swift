b9bf4ab252e7e802f6627270d0576d00b3deac63
dzhared <25894006+dzhared@users.noreply.github.com>
2023-03-16 19:04:37 -0500

    Add ImagePredictor class for ML identification

--- Green Thumb/Green Thumb/ImagePredictor.swift
+++ Green Thumb/Green Thumb/ImagePredictor.swift
@@ -1,159 +1,118 @@
-////
-////  ImagePredictor.swift
-////  Green Thumb
-////
-////  Created by Jared on 3/10/23.
-////
 //
-//// In Progress - code copied from Apple's sample code.
+//  ImageClassifier.swift
+//  Green Thumb
 //
-///*
-//See LICENSE folder for this sample’s licensing information.
+//  Created by Jared on 3/16/23.
 //
-//Abstract:
-//Makes predictions from images using the MobileNet model.
-//*/
-//
-//import Vision
-//import UIKit
-//
-///// A convenience class that makes image classification predictions.
-/////
-///// The Image Predictor creates and reuses an instance of a Core ML image classifier inside a ``VNCoreMLRequest``.
-///// Each time it makes a prediction, the class:
-///// - Creates a `VNImageRequestHandler` with an image
-///// - Starts an image classification request for that image
-///// - Converts the prediction results in a completion handler
-///// - Updates the delegate's `predictions` property
-///// - Tag: ImagePredictor
-//class ImagePredictor {
-//    /// - Tag: name
-//    static func createImageClassifier() -> VNCoreMLModel {
-//        // Use a default model configuration.
-//        let defaultConfig = MLModelConfiguration()
-//
-//        // Create an instance of the image classifier's wrapper class.
-//        let imageClassifierWrapper = try? MLModel(configuration: defaultConfig)
-//
-//        guard let imageClassifier = imageClassifierWrapper else {
-//            fatalError("App failed to create an image classifier model instance.")
-//        }
-//
-//        // Get the underlying model instance.
-//        let imageClassifierModel = imageClassifier.model
-//
-//        // Create a Vision instance using the image classifier's model instance.
-//        guard let imageClassifierVisionModel = try? VNCoreMLModel(for: imageClassifierModel) else {
-//            fatalError("App failed to create a `VNCoreMLModel` instance.")
-//        }
-//
-//        return imageClassifierVisionModel
-//    }
-//
-//    /// A common image classifier instance that all Image Predictor instances use to generate predictions.
-//    ///
-//    /// Share one ``VNCoreMLModel`` instance --- for each Core ML model file --- across the app,
-//    /// since each can be expensive in time and resources.
-//    private static let imageClassifier = createImageClassifier()
-//
-//    /// Stores a classification name and confidence for an image classifier's prediction.
-//    /// - Tag: Prediction
-//    struct Prediction {
-//        /// The name of the object or scene the image classifier recognizes in an image.
-//        let classification: String
-//
-//        /// The image classifier's confidence as a percentage string.
-//        ///
-//        /// The prediction string doesn't include the % symbol in the string.
-//        let confidencePercentage: String
-//    }
-//
-//    /// The function signature the caller must provide as a completion handler.
-//    typealias ImagePredictionHandler = (_ predictions: [Prediction]?) -> Void
-//
-//    /// A dictionary of prediction handler functions, each keyed by its Vision request.
-//    private var predictionHandlers = [VNRequest: ImagePredictionHandler]()
-//
-//    /// Generates a new request instance that uses the Image Predictor's image classifier model.
-//    private func createImageClassificationRequest() -> VNImageBasedRequest {
-//        // Create an image classification request with an image classifier model.
-//
-//        let imageClassificationRequest = VNCoreMLRequest(model: ImagePredictor.imageClassifier,
-//                                                         completionHandler: visionRequestHandler)
-//
-//        imageClassificationRequest.imageCropAndScaleOption = .centerCrop
-//        return imageClassificationRequest
-//    }
-//
-//    /// Generates an image classification prediction for a photo.
-//    /// - Parameter photo: An image, typically of an object or a scene.
-//    /// - Tag: makePredictions
-//    func makePredictions(for photo: UIImage, completionHandler: @escaping ImagePredictionHandler) throws {
-//        let orientation = CGImagePropertyOrientation(photo.imageOrientation)
-//
-//        guard let photoImage = photo.cgImage else {
-//            fatalError("Photo doesn't have underlying CGImage.")
-//        }
-//
-//        let imageClassificationRequest = createImageClassificationRequest()
-//        predictionHandlers[imageClassificationRequest] = completionHandler
-//
-//        let handler = VNImageRequestHandler(cgImage: photoImage, orientation: orientation)
-//        let requests: [VNRequest] = [imageClassificationRequest]
-//
-//        // Start the image classification request.
-//        try handler.perform(requests)
-//    }
-//
-//    /// The completion handler method that Vision calls when it completes a request.
-//    /// - Parameters:
-//    ///   - request: A Vision request.
-//    ///   - error: An error if the request produced an error; otherwise `nil`.
-//    ///
-//    ///   The method checks for errors and validates the request's results.
-//    /// - Tag: visionRequestHandler
-//    private func visionRequestHandler(_ request: VNRequest, error: Error?) {
-//        // Remove the caller's handler from the dictionary and keep a reference to it.
-//        guard let predictionHandler = predictionHandlers.removeValue(forKey: request) else {
-//            fatalError("Every request must have a prediction handler.")
-//        }
-//
-//        // Start with a `nil` value in case there's a problem.
-//        var predictions: [Prediction]? = nil
-//
-//        // Call the client's completion handler after the method returns.
-//        defer {
-//            // Send the predictions back to the client.
-//            predictionHandler(predictions)
-//        }
-//
-//        // Check for an error first.
-//        if let error = error {
-//            print("Vision image classification error...\n\n\(error.localizedDescription)")
-//            return
-//        }
-//
-//        // Check that the results aren't `nil`.
-//        if request.results == nil {
-//            print("Vision request had no results.")
-//            return
-//        }
-//
-//        // Cast the request's results as an `VNClassificationObservation` array.
-//        guard let observations = request.results as? [VNClassificationObservation] else {
-//            // Image classifiers, like MobileNet, only produce classification observations.
-//            // However, other Core ML model types can produce other observations.
-//            // For example, a style transfer model produces `VNPixelBufferObservation` instances.
-//            print("VNRequest produced the wrong result type: \(type(of: request.results)).")
-//            return
-//        }
-//
-//        // Create a prediction array from the observations.
-//        predictions = observations.map { observation in
-//            // Convert each observation into an `ImagePredictor.Prediction` instance.
-//            Prediction(classification: observation.identifier,
-//                       confidencePercentage: observation.confidencePercentageString)
-//        }
-//    }
-//}
-//
+
+import CoreML
+import SwiftUI
+import UIKit
+import Vision
+
+class ImagePredictor {
+    static func createImageClassifier() -> VNCoreMLModel {
+        // Use default model configuration
+        let defaultConfig = MLModelConfiguration()
+        
+        // Create instance of image classifier's wrapper class
+        let imageClassifierWrapper = try? PlantML(configuration: defaultConfig)
+        
+        guard let imageClassifier = imageClassifierWrapper else {
+            fatalError("App failed to create an image classifier model instance.")
+        }
+        
+        // Get underlying model instance
+        let imageClassifierModel = imageClassifier.model
+        
+        // Create a Vision instance using image classifier's model instance
+        guard let imageClassifierVisionModel = try? VNCoreMLModel(for: imageClassifierModel) else {
+            fatalError("App failed to create a `VNCoreMLModel` instance.")
+        }
+        
+        return imageClassifierVisionModel
+    }
+    
+    private static let imageClassifier = createImageClassifier()
+    
+    struct Prediction {
+        // Name of the object the classifier recognizes in an image
+        let classification: String
+    }
+    
+    // The function signature the caller must provide as a completion handler
+    typealias ImagePredictionHandler = (_ predictions: [Prediction]?) -> Void
+    
+    // Dictionary of prediction handler functions
+    private var predictionHandlers = [VNRequest: ImagePredictionHandler]()
+    
+    // Generate new request instance using Image Predictor's image classifier model
+    private func createImageClassificationRequest() -> VNImageBasedRequest {
+        // Create an image classification request with an image classifier model
+        let imageClassificationRequest = VNCoreMLRequest(model: ImagePredictor.imageClassifier, completionHandler: visionRequestHandler)
+        
+        imageClassificationRequest.imageCropAndScaleOption = VNImageCropAndScaleOption.centerCrop
+        return imageClassificationRequest
+    }
+    
+    // Generates an image classification prediction for a photo
+    func makePredictions(for photo: UIImage, completionHandler: @escaping ImagePredictionHandler) throws {
+        
+        guard let photoImage = photo.cgImage else {
+            fatalError("Photo doesn't have underlying CGImage.")
+        }
+        
+        let imageClassificationRequest = createImageClassificationRequest()
+        predictionHandlers[imageClassificationRequest] = completionHandler
+        
+        let handler = VNImageRequestHandler(cgImage: photoImage)
+        let requests: [VNRequest] = [imageClassificationRequest]
+        
+        // Start image classification request
+        try handler.perform(requests)
+    }
+    
+    private func visionRequestHandler(_ request: VNRequest, error: Error?) {
+        // Remove caller's handler from the dictionary and keep a reference to it
+        guard let predictionHandler = predictionHandlers.removeValue(forKey: request) else {
+            fatalError("Every request must have a prediction handler.")
+        }
+        
+        // Start with `nil` value in case there's a problem.
+        var predictions: [Prediction]? = nil
+        
+        // Call client's completion handler after method returns
+        defer {
+            // Send predictions back to client
+            predictionHandler(predictions)
+        }
+        
+        // Check for errors first
+        if let error = error {
+            print("Vision image classification error:\n\(error.localizedDescription)")
+            return
+        }
+        
+        // Check results aren't `nil`
+        if request.results == nil {
+            print("Vision request had no results.")
+            return
+        }
+        
+        // Cast request's results as `VNClassificationObservation` array.
+        guard let observations = request.results as? [VNClassificationObservation] else {
+            // Image classifier only produces classification observations
+            // This guard let statement can probably be removed
+            print("VNRequest produced the wrong type: \(type(of: request.results)).")
+            return
+        }
+        
+        // Create a prediction array from the observations
+        predictions = observations.map { observation in
+            // Convert each observation into an `ImagePredictor.Prediction` instance
+            Prediction(classification: observation.identifier)
+        }
+    }
+}
+
+
